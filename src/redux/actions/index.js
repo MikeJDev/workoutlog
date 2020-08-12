f89aa417985109b973569214f5d672de6fa55166
MikeJDev <ukmjanes@gmail.com>
2020-08-11 22:09:31 -0400

    added necessary skeleton for redux - stable

---
+++ src/redux/actions/index.js
@@ -0,0 +1,11 @@
+// import {
+//   ADD_TODO,
+//   RESET_TODOS,
+//   REMOVE_SPECIFIC_TODO,
+//   COMPLETE_SPECIFIC_TODO
+// } from '../action-types/index'
+
+
+// export function completeSpecificTodo(payload) {
+//   return { type: COMPLETE_SPECIFIC_TODO, payload}
+// }
